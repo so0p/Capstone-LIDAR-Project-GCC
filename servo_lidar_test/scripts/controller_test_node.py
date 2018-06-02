36c570f8de45c29abccb350e6bddadd40031780e
Vaneh Boghosian <vanehboghosian@gmail.com>
2018-06-02 02:24:04 +0000

    Add files via upload

--- servo_lidar_test/scripts/controller_test_node.py
+++ servo_lidar_test/scripts/controller_test_node.py
@@ -6,134 +6,141 @@ from std_msgs.msg import String
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 from servo_lidar_test.msg import Num
-from servo_lidar_test.msg import controller
+from servo_lidar_test.msg import pointCloud
 from servo_lidar_test.msg import servo
+from servo_lidar_test.srv import controller_client
 
-
+#Lidar And servo variables used in the controller node
 myRanges= []
 angleOfIncrement = 0
 startAngle = 0
+lidarAngleOffset = math.pi / 2.0 # Offseting lidar rays' angles by 90 degrees 
 
 
 servoAngle = 0
+servoOffset = 0.3316 # Offseting servo
+start_scan = 1
 
 
-# def callbackOne(msg):
-#     x = msg.pose.pose.position.x
-#     y = msg.pose.pose.position.y
-#     rospy.loginfo('x: {}, y: {}'. format(x, y))
 
-def callbackTwo(msg):
-     
-  
+#Lidar callback function
+def get_lidar_data(msg):
     global myRanges    # Needs to be global to be used in the main
     global angleOfIncrement
     global startAngle
+    global rangeTime
 
     myRanges = msg.ranges[:]
-   
     angleOfIncrement = msg.angle_increment
     startAngle = msg.angle_min
-
+    rangeTime = msg.header
     #rospy.loginfo(myRanges)
      
-
-def callbackThree(msg):
-
+#servo callback function
+def get_servo_data(msg):
     global servoAngle
-    servoAngle = msg.servoAngle
-     # Offseting servo
-
-
+    global servoTime
+    servoTime = msg.header
+    servoAngle = msg.servoAngle + servoOffset
+    print(servoTime)
+     
+# server callback funtion (To be developed)
+def handle_start(req):
+    start_scan = req.start 
+    print  (req.start)
+    return (req.start)
 
 
 def main():
 
-    rospy.init_node('controller_test')
-    
-    define_publish_rate = 6000 # Node Publish Rate
-
-
+    rospy.init_node('controller_test')  #Node name in ROS
+    s = rospy.Service('controller_service_name', controller_client, handle_start) # Service setup (to be developed)
+    controller_publish_rate = 10 #  contrller node Publish Rate Hz
+   
+    #t = input('hi, should I start Scanning?') #input from user node(to be developed)
     print("Starting the controller node.....")
+   
+    #coordinates = []  # Can be used if publishing one point at a time
+    x = []
+    y = []
+    z = []
 
-    count = 0 
-    coordinates = []
+    #Max and Min range to be pubblished by the controller node
+    RangeMax = 6     
+    RangeMin = 0.1
 
-    ######t = input('hi, should I start Scanning?') #####input from user node
+
+    
    
     #--------------------------------------------------
     # Publisher Setup
     #--------------------------------------------------
-    pub = rospy.Publisher('controller', controller, queue_size=10)
-    #pub2 = rospy.Publisher('custom_chatter', Num, queue_size=10)
-    
+    pub = rospy.Publisher('pointCloud', pointCloud, queue_size=10)
+   
 
-    
     #--------------------------------------------------
     # Subscriber Setup
     #--------------------------------------------------
-    #rospy.Subscriber("/odom", Odometry, callbackOne) For Odometry
-    rospy.Subscriber("/scan", LaserScan, callbackTwo)
-    rospy.Subscriber("servo_cmd", servo, callbackThree)
+    rospy.Subscriber("/scan", LaserScan, get_lidar_data)
+    rospy.Subscriber("servo_cmd", servo, get_servo_data)
     
-    
-
     
     rospy.sleep(1.)
-    rate = rospy.Rate(define_publish_rate)  # The while loop rate
+    rate = rospy.Rate(controller_publish_rate) 
     print("controller node started!")
     
-    msg = controller()
-    #msg.pointCloudRanges = []
+    msg = pointCloud()          #Copying poindCloud message into msg variable
+    #msg.pointCloudRanges = []  #Can be used if publishing one point at a time
 
-    #header 
+    #Defining the node header 
     msg.header.stamp = rospy.Time.now()
+    msg.header.frame_id = 'controller'
+
     
-    # msg2 = Num()
-    # msg2.num = 4
-    # msg2.name = "A"
 
     #--------------------------------------------------
     # Loop
     #--------------------------------------------------
     
     while not rospy.is_shutdown():
+        
+        rayAngle = startAngle  #Assinging the initial angle of the publishing array
+        index = 0              #counter
 
-        
-        
-        rayAngle = startAngle
-        index = 0
         for r in myRanges:
 
-            rayAngle = startAngle + index * angleOfIncrement + math.pi / 2.0
+            rayAngle = startAngle + index * angleOfIncrement + lidarAngleOffset
+            incrementedServoAngle = servoAngle + index * 0.000117 # interpolating the servo angle 
+
             index = index + 1
-             
-            #r = 1 
             
-            #------------To publish single coordinates of x,y,z------------
-            msg.y = r * math.cos(rayAngle) * math.cos(servoAngle + (33.75*math.pi/180))
-            msg.x = r * math.sin(rayAngle) * math.cos(servoAngle + (33.75*math.pi/180))
-            msg.z = r * math.sin(servoAngle + (33.75*math.pi/180))
-
-
              
+            if(r > RangeMax or r < RangeMin):     #If the lidar range is out of desired range assign zero to it
+                r = 0 
+
+            #------------Changing from polar to cartesian coordinates----------
+
+            #------------To publish single coordinates of x,y,z------------
+            # y.append(r * math.cos(servoAngle) * math.sin(rayAngle))
+            # z.append (r * math.sin(servoAngle) * math.sin(rayAngle))
+            # x.append(r * math.cos(rayAngle))
+
+            y.append(r * math.cos(incrementedServoAngle) * math.sin(rayAngle))
+            z.append (r * math.sin(incrementedServoAngle) * math.sin(rayAngle))
+            x.append(r * math.cos(rayAngle))
+            #--------------------------------------------------------------
+
+        msg.x = x[:]
+        msg.y = y[:]
+        msg.z = z[:]
+            
+        #Publishing the coordinates
+        pub.publish(msg)
+        del x[:]
+        del y[:]
+        del z[:]
 
 
-            ## -------- To publish an array of x,y,z------------------------
-            # y = r * math.cos(rayAngle) * math.cos(servoAngle + (33.75*math.pi/180))
-            # x = r * math.sin(rayAngle) * math.cos(servoAngle + (33.75*math.pi/180))
-            # z = - r * math.sin(servoAngle)
-
-            # coordinates.append([x,y,z])
-            # msg.pointCloudCoordinates = coordinates
-            pub.publish(msg)
-            #rospy.loginfo(rayAngle)
-
-      
-        
-        
-        
-        
     
         rate.sleep()
 
